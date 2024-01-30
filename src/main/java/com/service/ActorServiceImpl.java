d098bf77fa99b548f5be48cd7f94a75a310cebe8
Nicola Pirotta <nicolapirotta@gmail.com>
2024-01-30 17:10:56 +0100

    implementato l'ActorServiceImpl

--- src/main/java/com/service/ActorServiceImpl.java
+++ src/main/java/com/service/ActorServiceImpl.java
@@ -2,58 +2,80 @@ package com.service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.model.entity.Actor;
+import com.repository.ActorRepository;
+
+import jakarta.persistence.EntityExistsException;
+import jakarta.persistence.TransactionRequiredException;
 
 public class ActorServiceImpl implements ActorService {
+
+    @Autowired
+    private ActorRepository ar;
+
+    private Logger log = LoggerFactory.getLogger(this.getClass());
 
     @Override
     public void insertActor(Actor actor) {
         try {
-
-        } catch (Exception e) {
+            ar.insertActor(actor);
+            log.info("Actor inserted successfully");
+        } catch (EntityExistsException | IllegalArgumentException | TransactionRequiredException e) {
             e.printStackTrace();
-            System.err.println("Error in insertActor()");
+            log.error("Error in insertActor()");
         }
     }
 
     @Override
     public void updateActor(Actor actor) {
         try {
-
-        } catch (Exception e) {
+            ar.updateActor(actor);
+            log.info("Actor updated successfully");
+        } catch (IllegalArgumentException | TransactionRequiredException e) {
             e.printStackTrace();
-            System.err.println("Error in updateActor()");
+            log.error("Error in updateActor()");
         }
     }
 
     @Override
     public void deleteActor(Integer actorId) {
         try {
-
-        } catch (Exception e) {
+            ar.deleteActor(actorId);
+            log.info("Actor deleted successfully");
+        } catch (IllegalArgumentException | TransactionRequiredException e) {
             e.printStackTrace();
-            System.err.println("Error in deleteActor()");
+            log.error("Error in deleteActor()");
         }
     }
 
     @Override
     public Actor getActorById(Integer actorId) {
+        Actor actor = null;
         try {
-
-        } catch (Exception e) {
+            actor = ar.getActorById(actorId);
+        } catch (IllegalArgumentException e) {
             e.printStackTrace();
-            System.err.println("Error in getActorById()");
+            log.error("Error in getActorById()");
         }
+
+        return actor;
     }
 
     @Override
     public List<Actor> getAllActors() {
+        List<Actor> actors = null;
         try {
-
-        } catch (Exception e) {
+            actors = ar.getAllActors();
+        } catch (IllegalArgumentException e) {
             e.printStackTrace();
-            System.err.println("Error in getAllActors()");
+            log.error("Error in getAllActors()");
         }
+
+        return actors;
     }
 
 }
